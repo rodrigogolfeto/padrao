cf0980a100a4293bee53663067f9975cff29e2a7
Rodrigo Golfeto <rgolfeto@gmail.com>
2019-10-26 23:36:28 -0400

    Corrigindo erros em variáveis

--- includes/metas.inc.php
+++ includes/metas.inc.php
@@ -1,31 +1,31 @@
-<? if($keywordsPagina){ ?>
+<?php if(!empty($keywordsPagina)){ ?>
 	<meta name="keywords" content="<?= $keywordsPagina ?>" />
-<? }else { ?>
+<?php }else { ?>
 	<meta name="keywords" content="" />
-<? } ?>
-<? if($descriptionPagina){ ?>
+<?php } ?>
+<?php if(!empty($descriptionPagina)){ ?>
 	<meta name="description" content="<?= $descriptionPagina; ?>" />
-<? }else{ ?>
+<?php }else{ ?>
 	<meta name="description" content="" />
-<? } ?>
+<?php } ?>
 <!-- Metas Facebook -->
 <meta property="og:site_name" content="" />
-<? if(!$metasFacebook) { ?>    
+<?php if(empty($metasFacebook)) { ?>    
     <!-- <meta property="og:title" content="" /> <!-- Título da Página -->
     <meta property="og:description" content="" />
     <meta property="og:url" content="" />
     <meta property="og:image" content="" />
     <meta property="og:image:width" content="200" />
 	<meta property="og:image:height" content="200" />
-<? }else { echo $metasFacebook; } ?>
+<?php }else { echo $metasFacebook; } ?>
 <!-- Fim Metas Facebook -->
 <!-- Metas Twitter -->
 <meta name="twitter:card" value="summary" />
 <meta name="twitter:site" content="" /><!-- Conta do Twitter do site (incluindo arroba) -->
-<? if(!$metasTwitter) { ?>    
+<?php if(empty($metasTwitter)) { ?>    
     <!--<meta name="twitter:title" content="" /><!-- Título da página -->
     <!--<meta name="twitter:creator" content="" /> <!-- Conta do Twitter do autor do texto (incluindo arroba) -->
     <meta name="twitter:description" content="" />
     <meta name="twitter:image" content="" />
-<? }else { echo $metasTwitter; } ?>
+<?php }else { echo $metasTwitter; } ?>
 <!-- Fim Metas Twitter -->
